53dbab28cbceeb61241ab554ee1232edc2236d3c
LarisaSm <smirnova.larisa2020@yandex.ru>
2022-11-08 14:35:02 +0300

    Add FindIndex & FillArrayResult

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
 ﻿
+int size = 3;
+int lenght = 3;
+// string[] array = new string[] { "hello", "2", "world", ":-)" };
+string[] array = new string[] { "1234", "1567", "-2 ", "abra cadabra" };
+int[] count = new int[array.Length];
+System.Console.WriteLine("Исходный массив:");
+FillPrint.FillPrint.PrintMassive(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"Массив из элементов, длиной не более {lenght}:");
+FillPrint.FillPrint.PrintMassive(FillArrayResult(count, FindIndex(array, lenght, count)));
+
+int FindIndex(string[] arr, int lenght, int[] countArr)
+{
+
+  int k = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i].Length <= lenght)
+    {
+      countArr[k] = i;
+      k++;
+    }
+
+  }
+  return k;
+
+}
+
+
+string[] FillArrayResult(int[] countArr, int k)
+{
+
+  string[] result = new string[k];
+
+  for (int i = 0; i < result.Length; i++)
+  {
+    result[i] = array[countArr[i]];
+  }
+  return result;
+}
+
+
+
+
